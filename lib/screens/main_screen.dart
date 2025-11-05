ad1335f7ebd89e35f5aff868e3cdf90d93b89851
pratham-sarankar <pratham@sarankar.com>
2025-11-05 23:07:00 +0530

    Add transaction form navigation from floating action button

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -2,7 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:icons_plus/icons_plus.dart';
 import 'package:munshi/features/dashboard/screens/home_screen.dart';
 import 'package:munshi/features/settings/screens/settings_screen.dart';
+import 'package:munshi/features/transactions/providers/transaction_provider.dart';
+import 'package:munshi/features/transactions/screens/transaction_form_screen.dart';
 import 'package:munshi/features/transactions/screens/transactions_screen.dart';
+import 'package:provider/provider.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -53,6 +56,18 @@ class _MainScreenState extends State<MainScreen> {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
         onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) {
+                return TransactionFormScreen(
+                  onSubmit: (transaction) {
+                    final provider = context.read<TransactionProvider>();
+                    provider.addTransaction(transaction);
+                  },
+                );
+              },
+            ),
+          );
           // Handle floating action button press
         },
         shape: const CircleBorder(),
