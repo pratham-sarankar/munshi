a2a3884bed13a6edcd578be042364c84783606e3
pratham-sarankar <pratham@sarankar.com>
2025-11-10 01:58:55 +0530

    Add example app tested on iOS simulator.

--- packages/flutter_ocr/flutter_ocr/example/lib/main.dart
+++ packages/flutter_ocr/flutter_ocr/example/lib/main.dart
@@ -9,7 +9,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomePage());
+    return const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
@@ -21,13 +24,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  void _pickImageAndRecognizeText() async {
-    final ImagePicker picker = ImagePicker();
-    final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+  String _recognizedText = '';
+
+  Future<void> _pickImageAndRecognizeText() async {
+    final picker = ImagePicker();
+    final image = await picker.pickImage(source: ImageSource.gallery);
+
     if (image != null) {
-      final String? text = await FlutterOcrPlatform.instance
-          .recognizeTextFromFile(image.path);
-      // Do something with the recognized text
+      final text = await FlutterOcr.recognizeTextFromImage(image.path);
+      setState(() {
+        _recognizedText = text ?? '';
+      });
     }
   }
 
@@ -35,12 +42,35 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('FlutterOcr Example'),
+        title: const Text('OCR Example'),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [],
+        child: ListView(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 20,
+            vertical: 10,
+          ),
+          children: [
+            const SizedBox(height: 20),
+            Text(
+              'Recognized Text',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: _recognizedText.split('\n').map<Widget>((line) {
+                return Container(
+                  margin: const EdgeInsets.symmetric(vertical: 10),
+                  color: Colors.grey[200],
+                  child: Text(line),
+                );
+              }).toList(),
+            ),
+            ElevatedButton(
+              onPressed: _pickImageAndRecognizeText,
+              child: const Text('Pick Image and Recognize Text'),
+            ),
+          ],
         ),
       ),
     );
