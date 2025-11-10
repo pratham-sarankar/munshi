fd0eae98b104c2d1acd106567ddc85080cce3b4e
pratham-sarankar <pratham@sarankar.com>
2025-11-10 18:25:49 +0530

    Add dependency

--- packages/flutter_ocr/flutter_ocr_android/android/src/main/kotlin/com/example/verygoodcore/FlutterOcrPlugin.kt
+++ packages/flutter_ocr/flutter_ocr_android/android/src/main/kotlin/com/example/verygoodcore/FlutterOcrPlugin.kt
@@ -1,7 +1,5 @@
 package com.example.verygoodcore
 
-import androidx.annotation.NonNull
-
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
@@ -11,20 +9,20 @@ import io.flutter.plugin.common.MethodChannel.Result
 class FlutterOcrPlugin : FlutterPlugin, MethodCallHandler {
     private lateinit var channel: MethodChannel
 
-    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    override fun onAttachedToEngine(flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
         channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_ocr_android")
         channel.setMethodCallHandler(this)
     }
 
-    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    override fun onMethodCall(call: MethodCall, result: Result) {
         if (call.method == "getPlatformName") {
-            result.success("Android ${android.os.Build.VERSION.RELEASE}")            
+            result.success("Android ${android.os.Build.VERSION.RELEASE}")
         } else {
             result.notImplemented()
         }
     }
 
-    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         channel.setMethodCallHandler(null)
     }
 }
